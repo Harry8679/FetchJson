8687d8243ff52c84b4f0d472ed91e51db626aea5
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-03 19:03:08 +0100

    Handle Data

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios_1.default.get(url).then(function (response) {
-    console.log(response.data);
+    // console.log(response.data);
+    var _a = response.data, id = _a.id, title = _a.title, completed = _a.completed;
+    console.log("The Todo with ID: ".concat(id, "\n    Has a title of : ").concat(title, "\n    Is it finished ").concat(completed));
 });
--- index.ts
+++ index.ts
@@ -3,5 +3,9 @@ import axios from 'axios';
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 axios.get(url).then(response => {
-  console.log(response.data);
+  // console.log(response.data);
+  const { id, title, completed }  = response.data;
+  console.log(`The Todo with ID: ${id}
+    Has a title of : ${title}
+    Is it finished ${completed}`)
 });
